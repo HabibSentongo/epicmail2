66305861645e3c475f375d5564b3640af09ac11d
HabibSentongo <habibsentongo@gmail.com>
2019-03-28 13:37:29 +0300

    add api documentation and setup heroku [Delivers #164870559, #164955258]

--- app/views/view.py
+++ app/views/view.py
@@ -64,47 +64,47 @@ def user_signin():
 
 @app.route('/api/v2/groups', methods=['POST'])
 @jwt_required
-@swag_from('../apidocs/send_email.yml', methods=['POST'])
+@swag_from('../apidocs/create_group.yml', methods=['POST'])
 def create_group():
     return endpoint_function.create_group()
 
 @app.route('/api/v2/groups/<int:group_id>', methods=['DELETE'])
 @jwt_required
-@swag_from('../apidocs/delete_email.yml', methods=['DELETE'])
+@swag_from('../apidocs/delete_group.yml', methods=['DELETE'])
 def delete_group(group_id):
     return endpoint_function.delete_group(group_id)
 
 @app.route('/api/v2/groups/<int:group_id>/users', methods=['POST'])
 @jwt_required
-@swag_from('../apidocs/send_email.yml', methods=['POST'])
+@swag_from('../apidocs/add_member.yml', methods=['POST'])
 def add_member(group_id):
     return endpoint_function.add_member(group_id)
 
 @app.route('/api/v2/groups/<int:group_id>/users/<int:user_id>', methods=['DELETE'])
 @jwt_required
-@swag_from('../apidocs/send_email.yml', methods=['DELETE'])
+@swag_from('../apidocs/delete_member.yml', methods=['DELETE'])
 def delete_member(group_id, user_id):
     return endpoint_function.delete_member(group_id, user_id)
 
 @app.route('/api/v2/groups/<int:group_id>/name', methods=['PATCH'])
 @jwt_required
-@swag_from('../apidocs/send_email.yml', methods=['PATCH'])
+@swag_from('../apidocs/rename_group.yml', methods=['PATCH'])
 def rename_group(group_id):
     return endpoint_function.rename_group(group_id)
 
 @app.route('/api/v2/groups', methods=['GET'])
 @jwt_required
-@swag_from('../apidocs/send_email.yml', methods=['GET'])
+@swag_from('../apidocs/all_groups.yml', methods=['GET'])
 def all_groups():
     return endpoint_function.all_groups()
 
 @app.route('/api/v2/groups/<int:group_id>/messages', methods=['POST'])
 @jwt_required
-@swag_from('../apidocs/send_email.yml', methods=['POST'])
+@swag_from('../apidocs/send_group_email.yml', methods=['POST'])
 def send_group_mail(group_id):
     return endpoint_function.send_group_email(group_id)
 
 @app.route('/api/v2/auth/reset', methods=['POST'])
-@swag_from('../apidocs/send_email.yml', methods=['POST'])
+@swag_from('../apidocs/forgot_password.yml', methods=['POST'])
 def reset_password():
     return endpoint_function.forgot_password()
--- database/db.py
+++ database/db.py
@@ -9,13 +9,20 @@ class DBmigrate:
 
         if os.getenv('DB_NAME') == 'test_epicmail_db':
             self.db_name = 'test_epicmail_db'
+            self.user_password = ''
         else:
             self.db_name = 'epicmail_db'
+            self.user_password = 'root'
         print(self.db_name)
         self.user_name = 'postgres'
-        self.user_password = ''
+        self.user_password = 'root'
         self.host = '127.0.0.1'
         self.port = '5432'
+
+        if os.getenv('DB_NAME') == 'dcf13o9ukbb5mm':
+            self.db_name = 'dcf13o9ukbb5mm'
+            self.user_name = 'koggnwilbyhglb'
+            self.user_password = '58039f5f02d2a556522fccc99a424849bb01d98a470bbfd3162661d9a631dc8d'
         self.db_connect = psycopg2.connect(
             database=self.db_name, user=self.user_name, password=self.user_password, host=self.host, port=self.port)
         self.db_connect.autocommit = True
